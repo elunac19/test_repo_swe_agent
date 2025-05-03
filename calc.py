742c0a68f5ee35708f9672f446f863893d4d0790
mateobv07 <bernasconivargas.mateo@gmail.com>
2025-05-02 18:33:35 -0600

    llm changes

--- calc.py
+++ calc.py
@@ -6,5 +6,17 @@ def restar(a, b):
     """Returns the result of subtracting b from a."""
     return a - b
 
+def multiplicar(a, b):
+    """Returns the product of a and b."""
+    return a * b
+
+def dividir(a, b):
+    """Returns the result of dividing a by b."""
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
 print(sumar(10, 5))
-print(restar(10, 5)) 
+print(restar(10, 5))
+print(multiplicar(10, 5))
+print(dividir(10, 5))
